cb75b1f4af9aa854805b297fcb6faefb0d36eea8
İnci KÜÇÜK <56034341+incikucuk@users.noreply.github.com>
2024-06-25 12:52:08 +0300

    Time: 10 ms (88.93%) | Memory: 60.2 MB (32.20%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,18 +1,27 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        
-            int n = nums.length; // Step 1: Initialize variable to store length of nums
-            Set<Integer> set = new HashSet<>(); // Step 2: Create HashSet to store unique elements
-            for(int num : nums){
-                set.add(num); // Step 3: Add elements of nums to the HashSet
-            }
-            int m = set.size(); // Step 4: Get the size of the HashSet
-            if(n==m){    //Step 5: Compare lengths and return result
-                return false; //Return false if there were  no duplicates, true otherwise
-            }
-            return true;   
+        HashSet <Integer> occurrences = new HashSet<>();
+        for (int num: nums) {
+            if (occurrences.contains(num))
+                return true;
+            
+            occurrences.add(num);
+        }
+        return false; 
     }
+    
 }
+
+  /*int n = nums.length; // Step 1: Initialize variable to store length of nums
+    Set<Integer> set = new HashSet<>(); // Step 2: Create HashSet to store unique elements
+        for(int num : nums){
+            set.add(num); // Step 3: Add elements of nums to the HashSet
+        }
+        int m = set.size(); // Step 4: Get the size of the HashSet
+        if(n==m){    //Step 5: Compare lengths and return result
+            return false; //Return false if there were  no duplicates, true otherwise
+        }
+        return true; */
 
 /*class Solution {
     public boolean containsDuplicate(int[] nums) {
